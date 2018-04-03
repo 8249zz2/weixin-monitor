3ce0e605ff5169c06111a3b530d32ccd7c5689f7
xt12321 <1499410095@qq.com>
2018-04-03 22:26:58 +0800

    first

---
+++ pages/show/show.js
@@ -0,0 +1,35 @@
+// pages/show/show.js
+Page({ 
+  data: {
+  	imgUrls:[
+  		'../../images/1.png',
+  		'../../images/2.png',
+  		'../../images/3.png'
+  	],
+  	indicatorDots:false,
+  	autoplay:false,
+   } 
+   /*,
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+*/
+});
+
