95504608b8a87853bd6651f0f81bad58d2919594
8249zz2 <weixinzzw@163.com>
2018-04-12 19:08:43 +0800

    Modify the index page
    add some details , test the API  with  connect to the nodejs server

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    cycle: null,
+    monitor_number:null,
+    img_number:null,
+    href_number:null,
+    href_changed:null,
+    img_changed:null,
+    content_changed:null
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this;
+    
+    wx.request({
+      url: 'http://localhost:8080/?username=xt12321',
+      header: {
+        'content-type':'application/json'
+      },
+      success: function (res){
+        that.setData({
+        cycle : res.data[0].cycle,
+        href_number : res.data[0].href_number,
+        img_number  : res.data[0].img_number,
+        monitor_number : res.data[0].monitor_number,
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+
+
   }
 })
